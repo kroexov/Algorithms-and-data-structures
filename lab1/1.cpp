ba5830ce8978f7a455ce7c4aa077a2d29814a7e4
Ilya Korekhov <79146846+kroexov@users.noreply.github.com>
2022-02-19 17:05:54 +0300

    Update and rename 1 to 1.cpp

---
+++ lab1/1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a, b;
+    freopen("aplusb.in", "r", stdin);
+    freopen("aplusb.out", "w", stdout);
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
